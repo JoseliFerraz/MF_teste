1a3cf3f09fd92500f47bbe171f54d5078cb3df14
Joseli Moreira Ferraz <B222997918@ipea.gov.br>
2018-07-31 12:40:02 -0300

    alteracoes

--- codigomf.R
+++ codigomf.R
@@ -22,8 +22,10 @@ dados=X6_jun18
 head(dados)
 str(dados[10,2])
 names(dados)
+colnames(dados)=c("nomes","mesatual","mesaterior","anoanterior","variacao1","variacao2");names(dados)
+# Fixar todas as casas decimais dos dados
+options(digits=17)
 
-teste=as.numeric(dados[10,2])
 #-----------------------------------------------------------------------
 # Area de teste
 #Receitas=dados$X
@@ -37,42 +39,42 @@ teste=as.numeric(dados[10,2])
 #-----------------------------------------------------------------------
 
 
-n=length(dados$X)
-for(i in 8:n){ 
-  if((dados$X[i]=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")|
-     (dados$X[i]=="OUTRAS RECEITAS ADMINISTRADAS")){ #dados$X[i]=="CONTRIBUI플O PARA O FUNDAF")|(
-   
-     # Mes anterior
-    #a=dados$X.2[which(dados$X=="CONTRIBUI플O PARA O FUNDAF")]
-    b=dados$X.2[which(dados$X=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")]
-    c=dados$X.2[which(dados$X=="OUTRAS RECEITAS ADMINISTRADAS")]
-    if((dados$X[i]=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")|
-       (dados$X[i]=="OUTRAS RECEITAS ADMINISTRADAS")){#dados$X[i]=="CONTRIBUI플O PARA O FUNDAF")|(
-      
-    # Mes Atual
-    #d=dados$X.1[which(dados$X=="CONTRIBUI플O PARA O FUNDAF")]
-    e=dados$X.1[which(dados$X=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")]
-    f=dados$X.1[which(dados$X=="OUTRAS RECEITAS ADMINISTRADAS")]
-   }
-  }
-}
+# n=length(dados$nomes)
+# for(i in 8:n){ 
+#   if((dados$nomes[i]=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")|
+#      (dados$nomes[i]=="OUTRAS RECEITAS ADMINISTRADAS")){ #dados$X[i]=="CONTRIBUI??O PARA O FUNDAF")|(
+#    
+#      # Mes anterior
+#     #a=dados$X.2[which(dados$X=="CONTRIBUI??O PARA O FUNDAF")]
+#     b=dados$mesaterior[which(dados$nomes=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")]
+#     c=dados$mesaterior[which(dados$nomes=="OUTRAS RECEITAS ADMINISTRADAS")]
+#     if((dados$nomes[i]=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")|
+#        (dados$nomes[i]=="OUTRAS RECEITAS ADMINISTRADAS")){#dados$X[i]=="CONTRIBUI??O PARA O FUNDAF")|(
+#       
+#     # Mes Atual
+#     #d=dados$X.1[which(dados$X=="CONTRIBUI??O PARA O FUNDAF")]
+#     e=dados$mesatual[which(dados$nomes=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")]
+#     f=dados$mesatual[which(dados$nomes=="OUTRAS RECEITAS ADMINISTRADAS")]
+#    }
+#   }
+# }
 
 
-# para excel
+# Para excel
 n=length(dados$X0)
 for(i in 9:n){ 
   if((dados$X0[i]=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")|
-     (dados$X0[i]=="OUTRAS RECEITAS ADMINISTRADAS")){ #dados$X[i]=="CONTRIBUI플O PARA O FUNDAF")|(
+     (dados$X0[i]=="OUTRAS RECEITAS ADMINISTRADAS")){ #dados$X[i]=="CONTRIBUI??O PARA O FUNDAF")|(
     
     # Mes anterior
-    #a=dados$X.2[which(dados$X=="CONTRIBUI플O PARA O FUNDAF")]
+    #a=dados$X.2[which(dados$X=="CONTRIBUI??O PARA O FUNDAF")]
     b=dados$X2[which(dados$X=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")]
     c=dados$X2[which(dados$X=="OUTRAS RECEITAS ADMINISTRADAS")]
     if((dados$X0[i]=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")|
-       (dados$X0[i]=="OUTRAS RECEITAS ADMINISTRADAS")){#dados$X[i]=="CONTRIBUI플O PARA O FUNDAF")|(
+       (dados$X0[i]=="OUTRAS RECEITAS ADMINISTRADAS")){#dados$X[i]=="CONTRIBUI??O PARA O FUNDAF")|(
       
       # Mes Atual
-      #d=dados$X.1[which(dados$X=="CONTRIBUI플O PARA O FUNDAF")]
+      #d=dados$X.1[which(dados$X=="CONTRIBUI??O PARA O FUNDAF")]
       e=dados$X1[which(dados$X=="PSS - CONTRIB. DO PLANO DE SEGURIDADE DO SERVIDOR")]
       f=dados$X1[which(dados$X=="OUTRAS RECEITAS ADMINISTRADAS")]
     }
@@ -82,12 +84,15 @@ for(i in 9:n){
 
 
 # Armazenando e alterando os formatos dos dados
-a=as.numeric(levels(a))[a]
-b=as.numeric(levels(b))[b]
-c=as.numeric(levels(c))[c]
-d=as.numeric(levels(d))[d]
-e=as.numeric(levels(e))[e]
-f=as.numeric(levels(f))[f]
+# Esses dados nao esta sendo fornecidos pelo Ministerio : observar caso retornem , deve-se retirar os comentarios
+# a=as.numeric(a);d=as.numeric(d)
+
+
+b=as.numeric(b)
+c=as.numeric(c)
+
+e=as.numeric(e)
+f=as.numeric(f)
 
 # Criando a variavel soma e armazenando  
 mes_anterior=as.numeric(sum(b+c))
@@ -97,10 +102,11 @@ mes_atual=as.numeric(sum(e+f))
 dados=dados[-c(1,2,3,4,5,18,19,28,29,31,32,34,35,37,38,39,40),-5:-6]
 
 # Data frame para a soma dos meses
-soma=data.frame(0,mes_atual,mes_anterior,0);names(soma)<-c("X","X.1","X.2","X.3")
+soma=data.frame(0,mes_atual,mes_anterior,0);names(soma)<-c("nomes","mesatual","mesaterior","anoanterior")
 
 # Adicionando os resultados ao banco principal
 dados=data.frame(rbind(as.matrix(dados),as.matrix(soma)))
 
+# Salvando a atualizacao da serie : observe que deve-se inserir o nome.csv do mes de referencia
 write.csv2(dados,"Atualizacao.csv")
 
